b661c178d782df8afdf2c68f6b5560e370d24be6
Jeff Hackshaw <jeffrey.hackshaw@friends.dds.mil>
2020-02-03 14:03:29 -0500

    parameterize dhparam file and allow specification of full certificate file path

--- frontend/nginx/entrypoint.sh
+++ frontend/nginx/entrypoint.sh
@@ -6,6 +6,7 @@ ALLOWED_VARS+='$MAIN_DOMAIN'
 ALLOWED_VARS+='$API_DOMAIN'
 ALLOWED_VARS+='$CERT_FILE_NAME'
 ALLOWED_VARS+='$KEY_FILE_NAME'
+ALLOWED_VARS+='$DHPARAM_FILE_NAME'
 
 # rewrite nginx config based on environment
 # variables before starting
