ab6773954a025ceb65aa7c233efd87047a826c48
Garrett Haley <garrett.haley@friends.dds.mil>
2020-02-26 14:20:18 -0500

    Added circleCI deployment integrations with Terraform

--- deploy/staging/deploy.sh
+++ deploy/staging/deploy.sh
@@ -3,19 +3,25 @@ installTerraform(){
     set -eux
     wget https://releases.hashicorp.com/terraform/0.12.20/terraform_0.12.20_linux_amd64.zip;
     unzip terraform_0.12.20_linux_amd64.zip -d terraform;
-    sudo mv terraform/terraform /usr/local/bin;
-   
+    sudo mv terraform/terraform /usr/local/bin; 
 }
 configureAWS(){
     aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID};
     aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY};
+    $(aws ecr get-login --no-include-email --region us-gov-west-1);
 }
 pushFrontendToECR(){
     cd ../../frontend;
-    $(aws ecr get-login --no-include-email --region us-gov-west-1);
-    docker build -t frontend .;
-    docker tag frontend:latest 187588058266.dkr.ecr.us-gov-west-1.amazonaws.com/solo/frontend:latest;
-    docker push 187588058266.dkr.ecr.us-gov-west-1.amazonaws.com/solo/frontend:latest;
+    docker build -t solo-stage-frontend --build-arg API_DOMAIN=api.stage.solo.code.mil --build-arg AUTH_DOMAIN=auth.stage.solo.code.mil --build-arg API_PROTOCOL=https .;
+    docker tag solo-stage-frontend:latest 187588058266.dkr.ecr.us-gov-west-1.amazonaws.com/solo-stage-frontend:latest;
+    docker push 187588058266.dkr.ecr.us-gov-west-1.amazonaws.com/solo-stage-frontend:latest;
+    cd ../deploy/staging;
+}
+pushBackendToECR(){
+    cd ../../backend;
+    docker build -t solo-stage-backend .;
+    docker tag solo-stage-backend:latest 187588058266.dkr.ecr.us-gov-west-1.amazonaws.com/solo-stage-backend:latest;
+    docker push 187588058266.dkr.ecr.us-gov-west-1.amazonaws.com/solo-stage-backend:latest;
     cd ../deploy/staging;
 }
 runTerraform(){
@@ -23,25 +29,31 @@ runTerraform(){
     cd $dir_name;
     terraform init -backend-config=$dir_name-stage.config;
     terraform validate;
-    terraform plan -var-file=$dir_name-stage.tfvars;
-    terraform apply -auto-approve -var-file=$dir_name-stage.tfvars;
+    terraform plan -var-file=$dir_name-stage.tfvars -out=plan;
+    terraform apply "plan";
     cd ..;
 }
 createInfrastructure(){
-    runTerraform "infrastructure" #directory to traverse into from staging dir
+    runTerraform "infrastructure"; #directory to traverse into from staging dir
 }
 createPlatform(){
-    runTerraform "platform" #directory to traverse into from staging dir
+    runTerraform "platform"; #directory to traverse into from staging dir
 }
 createApplication(){
-    runTerraform "application" # directory to traverse into from staging dir
+    runTerraform "application"; # directory to traverse into from staging dir
 }
+createDatabase(){
+    runTerraform "database"; # directory to traverse into from staging dir
+}
+
 installTerraform;
 aws --version;
 terraform --version;
 configureAWS;
-#pushFrontendToECR;
+pushFrontendToECR;
+pushBackendToECR;
 createInfrastructure;
 createPlatform;
+createDatabase;
 createApplication;
 
