272d8da5d451baf5827465dc24b51140f05ae140
Victor G. Castro <victor.castro@friends.dds.mil>
2020-03-10 13:13:37 -0700

    Lint index

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -4,4 +4,3 @@ export { default as Table } from "./Table";
 export { default as Title } from "./Title";
 export { default as DocumentDetails } from "./DocumentDetails";
 export { default as DocumentStepper } from "./DocumentStepper";
-
