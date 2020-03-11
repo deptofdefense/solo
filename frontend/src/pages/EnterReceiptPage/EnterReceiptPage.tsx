f776cf39b871ae1d961b44578e9b5097b3d26aff
Victor G. Castro <victor.castro@friends.dds.mil>
2020-03-11 12:45:56 -0700

    Added Title Component to D6T page.

--- frontend/src/pages/EnterReceiptPage/EnterReceiptPage.tsx
+++ frontend/src/pages/EnterReceiptPage/EnterReceiptPage.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Title } from "components";
 
-const EnterReceiptPage: React.FC = () => <div>Enter Receipt</div>;
+const EnterReceiptPage: React.FC = () => {
+  return (
+    <div className="tablet:margin-x-8 overflow-x-auto">
+      <Title>D6T</Title>
+    </div>
+  );
+};
 
 export default EnterReceiptPage;
