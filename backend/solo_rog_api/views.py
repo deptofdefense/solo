04fec465c1c0ff617ace5ed9c836374aff58925f
Litthideth Phansiri <lit.phansiri@friends.dds.mil>
2020-03-24 21:15:23 +0900

    Update d6t code with comments

--- backend/solo_rog_api/views.py
+++ backend/solo_rog_api/views.py
@@ -8,7 +8,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.views import TokenObtainPairView
 from django_filters import rest_framework as filters
-from .models import Document, Dic
+from .models import Document, Dic, Status
 from .serializers import (
     TokenObtainSerializer,
     DocumentSerializer,
@@ -54,27 +54,65 @@ class DocumentList(generics.ListAPIView):
 
 
 class D6TSubmission(APIView):
-    def post(self, request):
+    def post(self, request) -> Response:
+
+        # initial queryset
+        documents = Document.objects.all()
+
+        # This will grab the dic code you wanted without creating more of the same one
+        d6t_code = Dic.objects.get(code="D6T")
+
         response_data = []
         serializers = []
+
         if len(request.data) > 0:
             for new_status in request.data:
                 try:
-                    document = Document.objects.get(sdn=new_status["sdn"])
+                    """The documents.get will query from the queryset we set up to called documents and return what 
+                    we set as the param. This way, we only have to query the database once for all the documents (it 
+                    will get slower when there are more rows in the future. """
+                    document = documents.get(sdn=new_status["sdn"])
                 except Document.DoesNotExist:
                     return Response(
                         "bad request: " + new_status["sdn"],
                         status=status.HTTP_400_BAD_REQUEST,
                     )
-                new_status["status_date"] = datetime.now()
-                new_status["dic"] = Dic.objects.get(code="D6T")
-                new_status["document"] = document.id
-                serializer = StatusSerializer(data=new_status)
-                if not serializer.is_valid():
-                    return Response(
-                        serializer.errors, status=status.HTTP_400_BAD_REQUEST
+
+                """               
+                This was something to grab the latest status based on the date vice the code because the latest one
+                ought to be a shipped status
+                """
+                latest_status = document.statuses.latest("status_date")
+
+                """
+                This is an attempt to only create a new status if and only if there was no d6t code. However, I have not
+                account for mutliple shipments.
+                """
+                if latest_status.dic.code != d6t_code.code:
+
+                    """
+                    This is to create a single status object
+                    """
+                    created_status = Status.objects.create(
+                        document_id=document.id,
+                        dic_id=d6t_code.id,
+                        status_date=datetime.now(),
+                        key_and_transmit_date=datetime.now(),
+                        projected_qty=latest_status.projected_qty,
+                        # something that is passed into from the frontend. As for now, its the same as projected_qty
+                        received_qty=new_status.projected_qty,
+                        # TODO: These two will depend on the Suppadd.
+                        # subinventory=,
+                        # locator=,
                     )
-                serializers.append(serializer)
+                    serializer = StatusSerializer(data=created_status)
+                    if not serializer.is_valid():
+                        return Response(
+                            serializer.errors, status=status.HTTP_400_BAD_REQUEST
+                        )
+                    serializers.append(serializer)
+                else:
+                    print("D6T for that document already exists")
             for serializer in serializers:
                 serializer.save()
                 response_data.append(serializer.data)
